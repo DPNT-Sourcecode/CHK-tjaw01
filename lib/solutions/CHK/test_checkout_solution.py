d4b35f7c0a7e329f56322bbb66daaf825ff93ac3
sbx_user1051 <sbx_user1051@169.254.152.181>
2022-03-04 12:52:30 +0000

    Fri Mar 04 12:52:30 UTC 2022

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -5,5 +5,13 @@ from checkout_solution import calculate_item_price
 class TestCheckout(unittest.TestCase):
     def test_calculate_item_price(self):
         self.assertAlmostEqual(calculate_item_price("A", 4), 180)
+        self.assertAlmostEqual(calculate_item_price("A", 6), 260)
+        self.assertAlmostEqual(calculate_item_price("B", 2), 45)
+        self.assertAlmostEqual(calculate_item_price("B", 3), 75)
+        self.assertAlmostEqual(calculate_item_price("C", 2), 40)
+        self.assertAlmostEqual(calculate_item_price("C", 3), 60)
+        self.assertAlmostEqual(calculate_item_price("D", 1), 15)
+        self.assertAlmostEqual(calculate_item_price("D", 4), 60)
+
 
 
