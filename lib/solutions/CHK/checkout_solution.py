06acd6a558db101a8d65197de9f749e1a0e5530e
sbx_user1051 <sbx_user1051@169.254.152.181>
2022-03-04 13:25:59 +0000

    Fri Mar 04 13:25:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 import json
 
+# We are going to sell a new item E.
+# Normally E costs 40, but if you buy 2 of Es you will get B free. How cool is
+# that ? Multi-priced items also seemed to work well so we should have more of
+# these.
+
+# Our price table and offers:
+# +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
+
 # Notes:
+#  - The policy of the supermarket is to always favor the customer when
+#  applying special offers.
+#  - All the offers are well balanced so that they can be safely combined.
 #  - For any illegal input return -1
-#
+
 # In order to complete the round you need to implement the following method:
 #      checkout(String) -> Integer
+
 # Where:
 #  - param[0] = a String containing the SKUs of all the products in the basket
 #  - @return = an Integer representing the total checkout value of the items
@@ -13,6 +33,66 @@ import json
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+# +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
+items = {
+    "A": {
+        "price": 50,
+        "offers": [
+            {
+                "required_item": "A",
+                "required_item_amount": 3,
+                "offer_item": "A",
+                "offer_price": 130
+            },
+            {
+                "required_item": "A",
+                "required_item_amount": 5,
+                "offer_item": "A",
+                "offer_price": 200
+            }
+        ]
+    },
+    "B": {
+        "price": 30,
+        "offers": [
+            {
+                "required_item": "B",
+                "required_item_amount": 2,
+                "offer_item": "B",
+                "offer_price": 45
+            }
+        ]
+    },
+    "C": {
+        "price": 20,
+        "offers": None
+    },
+    "D": {
+        "price": 15,
+        "offers": None
+    },
+    "E": {
+        "price": 40,
+        "offers": [
+            {
+                "required_item": "E",
+                "required_item_amount": 2,
+                "offer_item": "B",
+                "offer_price": 0
+            }
+        ]
+    },
+}
+
+
 def checkout(skus):
     skus_list = list(skus)
     valid_items = {"A", "B", "C", "D"}
@@ -28,28 +108,6 @@ def checkout(skus):
 
 
 def calculate_item_price(item, amount):
-    item_price = {
-        "A": {
-            "price": 50,
-            "offer_amount": 3,
-            "offer_price": 130
-        },
-        "B": {
-            "price": 30,
-            "offer_amount": 2,
-            "offer_price": 45
-        },
-        "C": {
-            "price": 20,
-            "offer_amount": None,
-            "offer_price": None
-        },
-        "D": {
-            "price": 15,
-            "offer_amount": None,
-            "offer_price": None
-        },
-    }
     item_price_json = json.loads(json.dumps(item_price))
     price = item_price_json[item]["price"]
     offer_amount = item_price_json[item]["offer_amount"]
@@ -62,4 +120,5 @@ def calculate_item_price(item, amount):
     else:
         total_item_price = int(price) * int(amount)
     return int(total_item_price)
+
 
